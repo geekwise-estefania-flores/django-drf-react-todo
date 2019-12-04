0a85098882d40ad16fe5b8bb348c4e893bb10f83
geekwise-estefania-flores <geekwise.estefania.flores@gmail.com>
2019-12-04 21:35:23 +0000

    debug: fixed issue with delete button

--- app/main/settings.py
+++ app/main/settings.py
@@ -127,4 +127,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals()) 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'https://3000-a4e6c3d4-5f88-4ee9-a45e-662be3d3372e.ws-us02.gitpod.io',
+)
--- app/main/urls.py
+++ app/main/urls.py
@@ -25,7 +25,7 @@ router.register(r'groups', views.GroupViewSet)
 router.register(r'todos', views.TodoViewSet, 'todo')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),          
     path('api/', include(router.urls))
